c14bd260700c4f1fd1a65e5de87a451dc5bd9c2a
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2022-08-30 18:32:43 +1000

    fix: add alacritty to link.sh

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-(cd ~/.config && ln -sf $HOME/_dotfiles/nvim)
 (cd ~ && ln -sf $HOME/_dotfiles/zsh/zshrc .zshrc)
+(cd ~/.config && ln -sf $HOME/_dotfiles/nvim)
+(cd ~/.config && ln -sf $HOME/_dotfiles/alacritty)
