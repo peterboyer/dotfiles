314e5ab22296de1798ee47598341060caa2b84fe
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2022-08-30 19:03:17 +1000

    feat: add ssh from zone linking

--- link.sh
+++ link.sh
@@ -3,3 +3,7 @@
 (cd ~ && ln -sf $HOME/_dotfiles/zsh/zshrc .zshrc)
 (cd ~/.config && ln -sf $HOME/_dotfiles/nvim)
 (cd ~/.config && ln -sf $HOME/_dotfiles/alacritty)
+
+if [[ -e ~/_zone/ssh ]]; then
+  (cd ~ && ln -sf $HOME/_zone/ssh .ssh)
+fi
