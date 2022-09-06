5ede51a12007d86512141c1b5e20a22293c9faaa
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2022-09-06 20:25:34 +1000

    feat(nvim): packer auto compile on plugins.lua save

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,6 +5,13 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 -- init/config plugins
 return require("packer").startup(function(use)
   -- packer
