1399fc8831f2bf9af222c0c7e74b72c075f086c4
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2022-09-09 16:50:17 +1000

    feat(tmux): add tmux config and link

--- link.sh
+++ link.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 (cd ~ && ln -sf $HOME/_dotfiles/zsh/zshrc .zshrc)
+(cd ~ && ln -sf $HOME/_dotfiles/tmux/tmux.conf .tmux.conf)
 (cd ~/.config && ln -sf $HOME/_dotfiles/nvim)
 (cd ~/.config && ln -sf $HOME/_dotfiles/alacritty)
 
