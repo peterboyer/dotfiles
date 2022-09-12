759ee2929d4983a221693239192f13f91228cec8
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2022-09-12 23:20:56 +1000

    fix: use relative paths for symlinks

--- link.sh
+++ link.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-(cd ~ && ln -sf $HOME/_dotfiles/zsh/zshrc .zshrc)
-(cd ~ && ln -sf $HOME/_dotfiles/tmux/tmux.conf .tmux.conf)
-(cd ~/.config && ln -sf $HOME/_dotfiles/nvim)
-(cd ~/.config && ln -sf $HOME/_dotfiles/alacritty)
+(cd ~ && ln -sf ./_dotfiles/zsh/zshrc .zshrc)
+(cd ~ && ln -sf ./_dotfiles/tmux/tmux.conf .tmux.conf)
+(cd ~/.config && ln -sf ./_dotfiles/nvim)
+(cd ~/.config && ln -sf ./_dotfiles/alacritty)
 
 if [[ -e ~/_zone/ssh ]]; then
-  (cd ~ && ln -sf $HOME/_zone/ssh .ssh)
+  (cd ~ && ln -sf ./_zone/ssh .ssh)
 fi
