3d6c81c97bd2ed62ef554ee55f0f38ebfb0319ab
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2022-12-23 20:29:51 +1100

    feat(packages): split into functions to be sourced or cli actions

--- packages.sh
+++ packages.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-# usage [<options>]:
-# --aur     also install aur packages
-
-# https://github.com/Jguer/yay
-if [[ "$(which yay &> /dev/null; echo $?)" != "0" ]]; then
-  git clone https://aur.archlinux.org/yay-bin.git $HOME/yay
-  (cd $HOME/yay && yes | makepkg -si --noconfirm)
-  rm -rf $HOME/yay
-fi
+# usage: [source] ./packages.sh [<function>]
+# <function>:
+#   bootstrap: install/configure everything (fresh system)
+#   update: only update
+#   install: update then install all stable packages
+#   install_aur: same as install + aur packages
+#   etc...
 
 PACKAGES=(
   htop
@@ -35,6 +33,7 @@ PACKAGES=(
   pulseaudio-bluetooth
   pulsemixer
   awesome
+  luarocks
   acpid
   slock
   xautolock
@@ -45,6 +44,7 @@ PACKAGES=(
   docker-compose
   calc
   peek
+  xcolor
   flameshot
   gimp
   pdfarranger
@@ -67,44 +67,139 @@ PACKAGES=(
   joyutils
   cmatrix
   AUR:slides-bin
+  AUR:timeshift-bin
 )
 
-packages=$((IFS=$'\n' && echo "${PACKAGES[*]}") | grep -v '^AUR:')
-packagesaur=$((IFS=$'\n' && echo "${PACKAGES[*]}") | grep '^AUR:' | sed 's/AUR://')
+BOOTSTRAP=()
 
-yay -Syu
-yay -S --needed --noconfirm ${packages[@]}
-if [[ "$@" =~ "--aur" ]]; then
-  yay -S --needed --noconfirm ${packagesaur[@]}
-fi
+yay_install() {
+  # https://github.com/Jguer/yay
+  if [[ "$(which yay &> /dev/null; echo $?)" != "0" ]]; then
+    git clone https://aur.archlinux.org/yay-bin.git $HOME/yay
+    (cd $HOME/yay && yes | makepkg -si --noconfirm)
+    rm -rf $HOME/yay
+  fi
+}
+BOOTSTRAP+=("yay_install")
+
+update() {
+  yay -Sy --noconfirm archlinux-keyring
+  yay -Syu --noconfirm
+}
+
+install_stable() {
+  packages=$((IFS=$'\n' && echo "${PACKAGES[*]}") | grep -v '^AUR:')
+  yay -S --needed --noconfirm ${packages[@]}
+}
+
+install_aur() {
+  packages=$((IFS=$'\n' && echo "${PACKAGES[*]}") | grep '^AUR:' | sed 's/AUR://')
+  yay -S --needed --noconfirm ${packages[@]}
+}
+
+install() {
+  update
+  install_stable
+}
+install_quick() {
+  # no update
+  install_stable
+}
+
+install_all() {
+  install
+  install_aur
+}
+BOOTSTRAP+=("install_all")
 
 # zsh
-if [[ "$(which zsh &> /dev/null; echo $?)" == "0" ]]; then
-  if [[ ! "$SHELL" =~ "zsh" ]]; then chsh -s $(which zsh); fi
-fi
+
+zsh_chsh() {
+  if [[ "$(which zsh &> /dev/null; echo $?)" == "0" ]]; then
+    if [[ ! "$SHELL" =~ "zsh" ]]; then chsh -s $(which zsh); fi
+  fi
+}
+
+zsh_init() {
+  zsh_chsh
+}
+BOOTSTRAP+=("zsh_init")
 
 # docker
-if [[ -z "$(systemctl status docker | grep running)" ]]; then
-  sudo systemctl enable docker --now;
-fi
-if [[ -z "$(cat /etc/group | grep docker)" ]]; then
-  sudo groupadd docker;
-fi
-if [[ -z "$(groups $USER | grep docker)" ]]; then
-  sudo usermod -aG docker $USER;
-fi
 
-# bluez
-if [[ -z "$(systemctl status bluetooth | grep running)" ]]; then
-  sudo systemctl enable bluetooth --now;
-fi
+docker_usergroup() {
+  if [[ -z "$(cat /etc/group | grep docker)" ]]; then
+    sudo groupadd docker;
+  fi
+  if [[ -z "$(groups $USER | grep docker)" ]]; then
+    sudo usermod -aG docker $USER;
+  fi
+}
 
-# acpid
-if [[ -z "$(systemctl status acpid | grep running)" ]]; then
-  sudo systemctl enable acpid --now;
-fi
+docker_enable() {
+  if [[ -z "$(systemctl status docker | grep running)" ]]; then
+    sudo systemctl enable docker --now;
+  fi
+}
+
+docker_init() {
+  docker_usergroup
+  docker_enable
+}
+BOOTSTRAP+=("docker_init")
+
+# bluez (bluetooth service)
+
+bluetooth_enable() {
+  if [[ -z "$(systemctl status bluetooth | grep running)" ]]; then
+    sudo systemctl enable bluetooth --now;
+  fi
+}
+
+bluetooth_init() {
+  bluetooth_enable
+}
+BOOTSTRAP+=("bluetooth_init")
+
+# acpid (power/battery information service)
+
+acpid_enable() {
+  if [[ -z "$(systemctl status acpid | grep running)" ]]; then
+    sudo systemctl enable acpid --now;
+  fi
+}
+
+acpid_init() {
+  acpid_enable
+}
+BOOTSTRAP+=("acpid_init")
 
 # awesome
-if [[ ! -d ~/.config/awesome/battery-widget ]]; then
-  git clone https://github.com/deficient/battery-widget.git ~/.config/awesome/battery-widget
+awesome_deps() {
+  if [[ ! -d ~/.config/awesome/battery-widget ]]; then
+    git clone https://github.com/deficient/battery-widget.git ~/.config/awesome/battery-widget
+  fi
+  if [[ ! -d ~/.config/awesome/pulseaudio_widget ]]; then
+    git clone https://github.com/stefano-m/awesome-pulseaudio_widget.git ~/.config/awesome/pulseaudio_widget
+  fi
+}
+
+awesome_init() {
+  awesome_deps
+}
+BOOTSTRAP+=("awesome_init")
+
+bootstrap() {
+  for item in "${BOOTSTRAP[@]}"; do
+    $item
+  done
+}
+
+if [[ -n "$1" ]]; then
+  if [[ `type -t $1` = "function" ]]; then
+    echo exec: function "$1"
+    $1
+  else
+    echo fatal: function "$1" is not recognised
+  fi
 fi
