2a6b6aa898a580c98c04c019a0f3cd6db5ec4be6
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2022-12-28 17:54:58 +1100

    fix(awesome): restore client borders

--- _/awesome/default/theme.lua
+++ _/awesome/default/theme.lua
@@ -25,9 +25,9 @@ theme.fg_urgent     = theme.fg_normal
 theme.fg_minimize   = theme.fg_normal
 
 theme.useless_gap   = dpi(5)
-theme.border_width  = dpi(0)
+theme.border_width  = dpi(2)
 theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
+theme.border_focus  = "#535353"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
