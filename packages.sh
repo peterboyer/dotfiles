f75ebcb5baee3723ad2c70b8cf1efa3fc1b54eb7
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2022-12-28 17:58:34 +1100

    format(packages): retab

--- packages.sh
+++ packages.sh
@@ -9,197 +9,197 @@
 #   etc...
 
 PACKAGES=(
-  htop
-  tmux
-  p7zip
-  rsync
-  bind
-  openssh
-  zsh
-  AUR:oh-my-zsh-git
-  exa
-  xclip
-  AUR:lf-bin
-  ripgrep
-  neovim
-  lazygit
-  xorg
-  xorg-xinit
-  xorg-xrandr
-  autorandr
-  bluez
-  bluez-utils
-  pulseaudio
-  pulseaudio-bluetooth
-  pulsemixer
-  awesome
-  luarocks
-  acpid
-  slock
-  xautolock
-  alacritty
-  ttf-jetbrains-mono
-  AUR:nvm
-  docker
-  docker-compose
-  calc
-  peek
-  xcolor
-  flameshot
-  gimp
-  pdfarranger
-  AUR:brave-bin
-  signal-desktop
-  AUR:flyctl-bin
-  AUR:spotify
-  udisks2
-  udiskie
-  ntfs-3g
-  ifuse
-  nemo
-  gthumb
-  vlc
-  linux-headers
-  v4l2loopback-dkms
-  gphoto2
-  obs-studio
-  AUR:obs-linuxbrowser-bin
-  joyutils
-  cmatrix
-  AUR:slides-bin
-  AUR:timeshift-bin
+	htop
+	tmux
+	p7zip
+	rsync
+	bind
+	openssh
+	zsh
+	AUR:oh-my-zsh-git
+	exa
+	neofetch
+	xclip
+	AUR:lf-bin
+	ripgrep
+	neovim
+	lazygit
+	xorg
+	xorg-xinit
+	xorg-xrandr
+	autorandr
+	bluez
+	bluez-utils
+	pulseaudio
+	pulseaudio-bluetooth
+	awesome
+	luarocks
+	acpid
+	slock
+	xautolock
+	alacritty
+	ttf-jetbrains-mono
+	AUR:nvm
+	docker
+	docker-compose
+	calc
+	peek
+	xcolor
+	flameshot
+	gimp
+	pdfarranger
+	AUR:brave-bin
+	signal-desktop
+	AUR:flyctl-bin
+	AUR:spotify
+	udisks2
+	udiskie
+	ntfs-3g
+	ifuse
+	nemo
+	gthumb
+	vlc
+	linux-headers
+	v4l2loopback-dkms
+	gphoto2
+	obs-studio
+	AUR:obs-linuxbrowser-bin
+	joyutils
+	cmatrix
+	AUR:slides-bin
+	AUR:timeshift-bin
 )
 
 BOOTSTRAP=()
 
 yay_install() {
-  # https://github.com/Jguer/yay
-  if [[ "$(which yay &> /dev/null; echo $?)" != "0" ]]; then
-    git clone https://aur.archlinux.org/yay-bin.git $HOME/yay
-    (cd $HOME/yay && yes | makepkg -si --noconfirm)
-    rm -rf $HOME/yay
-  fi
+	# https://github.com/Jguer/yay
+	if [[ "$(which yay &> /dev/null; echo $?)" != "0" ]]; then
+		git clone https://aur.archlinux.org/yay-bin.git $HOME/yay
+		(cd $HOME/yay && yes | makepkg -si --noconfirm)
+		rm -rf $HOME/yay
+	fi
 }
 BOOTSTRAP+=("yay_install")
 
 update() {
-  yay -Sy --noconfirm archlinux-keyring
-  yay -Syu --noconfirm
+	yay -Sy --noconfirm archlinux-keyring
+	yay -Syu --noconfirm
 }
 
 install_stable() {
-  packages=$((IFS=$'\n' && echo "${PACKAGES[*]}") | grep -v '^AUR:')
-  yay -S --needed --noconfirm ${packages[@]}
+	packages=$((IFS=$'\n' && echo "${PACKAGES[*]}") | grep -v '^AUR:')
+	yay -S --needed --noconfirm ${packages[@]}
 }
 
 install_aur() {
-  packages=$((IFS=$'\n' && echo "${PACKAGES[*]}") | grep '^AUR:' | sed 's/AUR://')
-  yay -S --needed --noconfirm ${packages[@]}
+	packages=$((IFS=$'\n' && echo "${PACKAGES[*]}") | grep '^AUR:' | sed 's/AUR://')
+	yay -S --needed --noconfirm ${packages[@]}
 }
 
 install() {
-  update
-  install_stable
+	update
+	install_stable
 }
 install_quick() {
-  # no update
-  install_stable
+	# no update
+	install_stable
 }
 
 install_all() {
-  install
-  install_aur
+	install
+	install_aur
 }
 BOOTSTRAP+=("install_all")
 
 # zsh
 
 zsh_chsh() {
-  if [[ "$(which zsh &> /dev/null; echo $?)" == "0" ]]; then
-    if [[ ! "$SHELL" =~ "zsh" ]]; then chsh -s $(which zsh); fi
-  fi
+	if [[ "$(which zsh &> /dev/null; echo $?)" == "0" ]]; then
+		if [[ ! "$SHELL" =~ "zsh" ]]; then chsh -s $(which zsh); fi
+	fi
 }
 
 zsh_init() {
-  zsh_chsh
+	zsh_chsh
 }
 BOOTSTRAP+=("zsh_init")
 
 # docker
 
 docker_usergroup() {
-  if [[ -z "$(cat /etc/group | grep docker)" ]]; then
-    sudo groupadd docker;
-  fi
-  if [[ -z "$(groups $USER | grep docker)" ]]; then
-    sudo usermod -aG docker $USER;
-  fi
+	if [[ -z "$(cat /etc/group | grep docker)" ]]; then
+		sudo groupadd docker;
+	fi
+	if [[ -z "$(groups $USER | grep docker)" ]]; then
+		sudo usermod -aG docker $USER;
+	fi
 }
 
 docker_enable() {
-  if [[ -z "$(systemctl status docker | grep running)" ]]; then
-    sudo systemctl enable docker --now;
-  fi
+	if [[ -z "$(systemctl status docker | grep running)" ]]; then
+		sudo systemctl enable docker --now;
+	fi
 }
 
 docker_init() {
-  docker_usergroup
-  docker_enable
+	docker_usergroup
+	docker_enable
 }
 BOOTSTRAP+=("docker_init")
 
 # bluez (bluetooth service)
 
 bluetooth_enable() {
-  if [[ -z "$(systemctl status bluetooth | grep running)" ]]; then
-    sudo systemctl enable bluetooth --now;
-  fi
+	if [[ -z "$(systemctl status bluetooth | grep running)" ]]; then
+		sudo systemctl enable bluetooth --now;
+	fi
 }
 
 bluetooth_init() {
-  bluetooth_enable
+	bluetooth_enable
 }
 BOOTSTRAP+=("bluetooth_init")
 
 # acpid (power/battery information service)
 
 acpid_enable() {
-  if [[ -z "$(systemctl status acpid | grep running)" ]]; then
-    sudo systemctl enable acpid --now;
-  fi
+	if [[ -z "$(systemctl status acpid | grep running)" ]]; then
+		sudo systemctl enable acpid --now;
+	fi
 }
 
 acpid_init() {
-  acpid_enable
+	acpid_enable
 }
 BOOTSTRAP+=("acpid_init")
 
 # awesome
 awesome_deps() {
-  if [[ ! -d ~/.config/awesome/battery-widget ]]; then
-    git clone https://github.com/deficient/battery-widget.git ~/.config/awesome/battery-widget
-  fi
-  if [[ ! -d ~/.config/awesome/pulseaudio_widget ]]; then
-    git clone https://github.com/stefano-m/awesome-pulseaudio_widget.git ~/.config/awesome/pulseaudio_widget
-  fi
+	if [[ ! -d ~/.config/awesome/battery-widget ]]; then
+		git clone https://github.com/deficient/battery-widget.git ~/.config/awesome/battery-widget
+	fi
+	if [[ ! -d ~/.config/awesome/pulseaudio_widget ]]; then
+		git clone https://github.com/stefano-m/awesome-pulseaudio_widget.git ~/.config/awesome/pulseaudio_widget
+	fi
 }
 
 awesome_init() {
-  awesome_deps
+	awesome_deps
 }
 BOOTSTRAP+=("awesome_init")
 
 bootstrap() {
-  for item in "${BOOTSTRAP[@]}"; do
-    $item
-  done
+	for item in "${BOOTSTRAP[@]}"; do
+		$item
+	done
 }
 
 if [[ -n "$1" ]]; then
-  if [[ `type -t $1` = "function" ]]; then
-    echo exec: function "$1"
-    $1
-  else
-    echo fatal: function "$1" is not recognised
-  fi
+	if [[ `type -t $1` = "function" ]]; then
+		echo exec: function "$1"
+		$1
+	else
+		echo fatal: function "$1" is not recognised
+	fi
 fi
