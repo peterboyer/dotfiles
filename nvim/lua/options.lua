13f6b0a323ce4565428bc818782fd2da0f3956af
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-01-09 18:14:25 +1100

    feat(nvim): options, colorcolumn scroll signcolumn folds

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,20 +4,19 @@ vim.o.termguicolors = false
 vim.o.mouse = false
 vim.o.guicursor = ""
 vim.o.cursorline = true
+vim.o.colorcolumn = "80"
 
 vim.o.number = true
 vim.o.relativenumber = true
--- auto hide/show line numbers on blur/focus
-vim.cmd([[
-	autocmd WinEnter * setlocal nu rnu
-	autocmd WinLeave * setlocal nonu nornu
-]])
--- auto hide line numbers for terminals
+vim.o.signcolumn = "yes"
 vim.cmd("autocmd TermOpen * setlocal nonu nornu")
 
--- textwrap
+-- wrap
 vim.o.wrap = false
 vim.o.autoread = true
+
+-- scroll
+vim.o.scrolloff = 8
 
 -- tabs
 vim.o.tabstop = 4
@@ -26,17 +25,14 @@ vim.o.shiftwidth = 0
 -- search
 vim.o.ignorecase = true
 
--- autocomplete
--- vim.o.wildmode = "longest,list"
-
 -- windows
 vim.o.splitright = true
 vim.o.splitbelow = true
 
 -- folding
--- vim.o.foldenable = true
--- vim.o.foldmethod = "syntax"
--- vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.foldmethod = "syntax"
+vim.o.foldlevelstart = 99
 
 -- whitespace
 vim.o.list = true
