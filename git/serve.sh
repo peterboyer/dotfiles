74fa83256f9a76508a16d373e172dc8c7e33d759
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-01-09 18:22:59 +1100

    fix(git): serve mkdir

--- git/serve.sh
+++ git/serve.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 REPOS=$(dirname $0)/repos
-if [[ "${REPOS:0:1}" != "/" ]]; then REPOS=$PWD/${REPOS:2}; fi
 mkdir -p $REPOS
 docker run --rm -p 8080:80 -v $REPOS:/var/lib/git cirocosta/gitserver-http:formatting
