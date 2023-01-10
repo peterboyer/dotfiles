4432eed3f231c715c1baef0ad20c1cd9b1bef44d
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-01-10 23:04:49 +1100

    feat(nvim): telescope, revise keybinds

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,18 +1,23 @@
 require("telescope").setup {
 	defaults = {
-		path_display = { "truncate" },
+		path_display = { truncate = 3 },
 		file_ignore_patterns = {
 			"%.git/",
 			"%.yarn/",
+			"%node_modules/",
 		},
 	},
 	pickers = {
+		git_files = {
+			show_untracked = true,
+			use_git_root = false,
+		},
 		find_files = {
 			hidden = true, -- dotfiles
 			follow = true, -- symlinks
+			no_ignore = true,
 		},
 		buffers = {
-			show_all_buffers = true,
 			sort_lastused = true,
 			mappings = {
 				i = {
@@ -20,13 +25,17 @@ require("telescope").setup {
 				},
 			},
 		},
+		oldfiles = {
+		  only_cwd = true,
+		},
 	},
 }
 
-local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<leader>p", telescope.find_files)
-vim.keymap.set("n", "<leader>P", telescope.oldfiles)
-vim.keymap.set("n", "<leader>b", telescope.buffers)
-vim.keymap.set("n", "<leader>fg", telescope.live_grep)
-vim.keymap.set("n", "<leader>fh", telescope.help_tags)
-vim.keymap.set("n", "<leader>fs", telescope.git_status)
+local t = require("telescope.builtin")
+vim.keymap.set("n", "<leader>p", t.git_files)
+vim.keymap.set("n", "<leader>P", t.find_files)
+vim.keymap.set("n", "<leader>b", t.buffers)
+vim.keymap.set("n", "<leader>o", t.oldfiles)
+vim.keymap.set("n", "<leader>fg", t.live_grep)
+vim.keymap.set("n", "<leader>fh", t.help_tags)
+vim.keymap.set("n", "<leader>fs", t.git_status)
