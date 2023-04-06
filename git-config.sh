50b7b030e418bdd1d1fb26b13a85f31ad69dbd9b
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-04-06 21:17:53 +1000

    feat(git): default to use latest commit, added --existing flag

--- git-config.sh
+++ git-config.sh
@@ -3,25 +3,19 @@
 if [[ "$@" =~ "--help" ]]; then
   echo "usage: [<options>]"
   echo "  --ask         read name/email from prompts"
-  echo "  --repo        read name/email from HEAD commit"
+  echo "  --existing    read name/email from HEAD commit"
   echo "  --lfs         include git-lfs filter options"
   echo "  --alias       include helper command aliases"
   exit
 fi
 
-GIT_CONFIG_NAME="$(git config --global user.name)"
-GIT_CONFIG_EMAIL="$(git config --global user.email)"
-
-GIT_REPO_NAME="$(git log -n 1 --format='%an')"
-GIT_REPO_EMAIL="$(git log -n 1 --format='%aE')"
-
-if [[ "$@" =~ "--repo" ]]; then
-  GIT_CONFIG_NAME=""
-  GIT_CONFIG_EMAIL=""
+if [[ "$@" =~ "--existing" ]]; then
+	GIT_CONFIG_NAME="$(git config --global user.name)"
+	GIT_CONFIG_EMAIL="$(git config --global user.email)"
 fi
 
-GIT_NAME="${GIT_CONFIG_NAME:-$GIT_REPO_NAME}"
-GIT_EMAIL="${GIT_CONFIG_EMAIL:-$GIT_REPO_EMAIL}"
+GIT_NAME="${GIT_CONFIG_NAME:-$(git log -n 1 --format='%an')}"
+GIT_EMAIL="${GIT_CONFIG_EMAIL:-$(git log -n 1 --format='%aE')}"
 
 if [[ "$@" =~ "--ask" ]]; then
   GIT_NAME=""
@@ -50,15 +44,6 @@ cat << EOF >> ~/.gitconfig
 	smudge = git-lfs smudge -- %f
 	process = git-lfs filter-process
 	required = true
-EOF
-fi
-
-if [[ "$@" =~ "--alias" ]]; then
-cat << EOF >> ~/.gitconfig
-[alias]
-	# stash staged
-	ss = "!f() { git stash -- \$(git diff --staged --name-only); }; f"
-	serve = "!f() { zsh $HOME/_dotfiles/git/serve.sh; }; f"
 EOF
 fi
 
