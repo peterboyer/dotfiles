2b17e03850a03fca43fbf8853eb8c5a633e03e56
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-04-06 20:52:14 +1000

    feat(packages): migrate to opensuse

--- link.sh
+++ link.sh
@@ -4,45 +4,10 @@ main() {
 	link $HOME/_dotfiles/zsh/zshrc                      $HOME/.@
 	link $HOME/_dotfiles/tmux/tmux.conf                 $HOME/.@
 	link $HOME/_dotfiles/nvim                           $HOME/.config/@
-	link $HOME/_dotfiles/kitty                          $HOME/.config/@
 
 	link $HOME/_zone/ssh                                $HOME/.@
 	link $HOME/_zone/_dotfiles.private/gphoto           $HOME/.@
 	link $HOME/_zone/_dotfiles.private/obs/basic        $HOME/.config/obs-studio/@
-
-	link $HOME/_dotfiles/_arch/xinitrc                  $HOME/.@
-	link $HOME/_dotfiles/_arch/xresources               $HOME/.@
-	link $HOME/_dotfiles/awesome                        $HOME/.config/@
-	link $HOME/_zone/wallpapers/valley.jpg              $HOME/.config/wallpaper
-	link $HOME/_zone/_dotfiles.private/autorandr        $HOME/.config/@
-
-	link $HOME/_dotfiles/_arch/issue                    /etc/@ --sudo
-	link $HOME/_dotfiles/_arch/vkeyboard.map            /usr/share/kbd/keymaps/@ --sudo
-	link $HOME/_dotfiles/_arch/vconsole.conf            /etc/@ --sudo
-	link $HOME/_dotfiles/_arch/fonts.conf               $HOME/.config/fontconfig/@
-	link $HOME/_dotfiles/_arch/xkeyboard                /etc/default/keyboard
-	link $HOME/_dotfiles/_arch/udev/60-keyboard.rules   /etc/udev/rules.d/@ --sudo
-
-	link $HOME/_zone/fonts/CousineMono                  /usr/local/share/fonts/@ --sudo
-	link $HOME/_zone/fonts/BerkeleyMono                 /usr/local/share/fonts/@ --sudo
-
-	return
-
-	link $HOME/_dotfiles/_arch/networkmanager/dns-servers.conf /etc/NetworkManager/conf.d/@ --sudo
-
-	link $HOME/_dotfiles/_arch/bashrc $HOME/.@
-	link $HOME/_dotfiles/_arch/bash_profile $HOME/.@
-	link $HOME/_dotfiles/_arch/bash_logout $HOME/.@
-	link $HOME/_dotfiles/_arch/zprofile $HOME/.@
-
-	link $HOME/_dotfiles/_arch/xinitrc $HOME/.@
-	link $HOME/_dotfiles/_arch/xmodmap $HOME/.Xmodmap
-	link $HOME/_dotfiles/_arch/xinitrc.d/51-xrdb.sh /etc/X11/xinit/xinitrc.d/@ --sudo
-	link $HOME/_dotfiles/_arch/xinitrc.d/52-xmodmap.sh /etc/X11/xinit/xinitrc.d/@ --sudo
-	link $HOME/_dotfiles/_arch/xinitrc.d/71-udiskie.sh /etc/X11/xinit/xinitrc.d/@ --sudo
-	link $HOME/_dotfiles/_arch/xinitrc.d/72-autorandr.sh /etc/X11/xinit/xinitrc.d/@ --sudo
-	link $HOME/_dotfiles/_arch/xinitrc.d/81-xautolock.sh /etc/X11/xinit/xinitrc.d/@ --sudo
-
 }
 
 # link <src> <dest> [--sudo]
--- packages.sh
+++ packages.sh
@@ -1,5 +1,71 @@
 #!/bin/bash
 
+# == hardware ==
+# sudo zypper install alsa-firmware
+# sudo zypper install sof-firmware
+
+# == shell ==
+# sudo zypper install zsh
+# chsh -s /bin/zsh
+# export OMZ_DIR="/usr/share/oh-my-zsh-git" && (
+	# sudo git clone https://github.com/ohmyzsh/ohmyzsh.git "$OMZ_DIR"
+	# sudo chown -R $USER:$USER $OMZ_DIR
+# )
+# sudo zypper install exa
+# sudo zypper install tmux
+# sudo zypper install fzf
+
+# == general ==
+# sudo zypper install git
+# sudo zypper install neofetch
+
+# == lazygit ==
+# export LAZYGIT_DIR="/usr/share/lazygit-bin" && (
+	# sudo mkdir -p $LAZYGIT_DIR
+	# sudo chown -R $USER:$USER $LAZYGIT_DIR
+	# cd "$LAZYGIT_DIR"
+	# curl -L https://github.com/jesseduffield/lazygit/releases/download/v0.37.0/lazygit_0.37.0_Linux_x86_64.tar.gz | tar -xz
+	# sudo ln -s -t /usr/bin /usr/share/lazygit-bin/lazygit
+# )
+
+# == neovim ==
+# sudo zypper install neovim
+# sudo zypper install gcc # for treesitter
+# sudo zypper install gcc-c++ # for treesitter
+# sudo zypper install ripgrep # for telescope
+# export NVM_DIR="/usr/share/nvm-git" && (
+	# sudo git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+	# sudo chown -R $USER:$USER $NVM_DIR
+	# cd "$NVM_DIR"
+	# git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+# ) && \. "$NVM_DIR/nvm.sh"
+
+# == brave ==
+# sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+# sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+# sudo zypper install brave-browser
+
+# == vlc ==
+sudo zypper install vlc
+
+# == packman ==
+# sudo zypper addrepo -cfp 90 http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+# sudo zypper dist-upgrade --from packman --allow-vendor-change
+
+# == flatpak ==
+# sudo zypper install flatpak
+
+# == spotify ==
+# flatpak install com.spotify.Client
+
+# == discord ==
+# flatpak install com.discordapp.Discord
+
+# == obs-studio ==
+# flatpak install com.obsproject.Studio
+
+exit
+
 PACKAGES=(
 	openssh
 	udisks2
