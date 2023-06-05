bd79ee653265fdd5ed11e963e92ae5304d0181e1
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-06-05 17:32:05 +1000

    feat(nvim): switch to oil from nvim-tree

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,16 @@
+local keymap = function()
+	vim.keymap.set("n", "-", require("oil").open)
+end
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+return {
+	{
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup({})
+			keymap()
+		end,
+	},
+}
