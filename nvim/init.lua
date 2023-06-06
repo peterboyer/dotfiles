327dca48b6c51dcb1ed61fc1ae5450c90ba855d0
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-06-07 09:22:00 +1000

    style(nvim): stylua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,16 +3,18 @@ require("config.options")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-	"--filter=blob:none",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		"--filter=blob:none",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
 	change_detection = { notify = false },
-	ui = { border = "rounded" }
+	ui = { border = "rounded" },
 })
