9fe33fdeae7c0717ba9668100abefd119e1dc13d
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-12-11 13:02:27 +1100

    feat(nvim): gitgutter, use block as indicator

--- nvim/lua/plugins/gitgutter.lua
+++ nvim/lua/plugins/gitgutter.lua
@@ -1,7 +1,12 @@
 -- @workaround
--- Setting any `sign_*` options only seems to work _before_ the plugin is initialised.
+-- Config must happen _before_ the plugin is initialised.
 vim.cmd([[
-	let g:gitgutter_sign_removed = "-"
+	let g:gitgutter_sign_added                   = '█'
+	let g:gitgutter_sign_modified                = '█'
+	let g:gitgutter_sign_removed                 = '█'
+	let g:gitgutter_sign_removed_first_line      = '█'
+	let g:gitgutter_sign_removed_above_and_below = '█'
+	let g:gitgutter_sign_modified_removed        = '█'
 ]])
 
 return {
