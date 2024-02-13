d1f525fa2724fa78b3e69a2e6004bfdcbfed9f39
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-02-13 18:51:38 +1100

    fix(nvim): only use prettier, ignore stylelint

--- nvim/lua/plugins/neoformat.lua
+++ nvim/lua/plugins/neoformat.lua
@@ -4,6 +4,7 @@ return {
 		config = function()
 			vim.cmd([[
 				let g:neoformat_try_node_exe = 1
+				let g:neoformat_enabled_css = ['prettier']
 				augroup fmt
 					autocmd!
 					autocmd BufWritePre * Neoformat
