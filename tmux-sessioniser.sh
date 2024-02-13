17b3b179c719bdd3efb046f4f21a62a2541e983a
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-02-13 18:52:12 +1100

    refactor(zsh): tmux-sessioniser

--- tmux-sessioniser.sh
+++ tmux-sessioniser.sh
@@ -1,31 +1,19 @@
 #!/usr/bin/env zsh
 
-if [[ $# -eq 1 ]]; then
-	selected=$1
-else
-	dirs=$(find ~/_dev -mindepth 1 -maxdepth 1 -type d)
-	dirs+="\n$HOME/_dotfiles"
-	selected=$(echo $dirs | fzf)
-fi
+dirs="$(find ~/_dev -mindepth 1 -maxdepth 1 -type d)"
+dirs+="\n$HOME/_dotfiles"
 
-if [[ -z $selected ]]; then
-	exit 1
-fi
+selected=${1:-$(echo $dirs | fzf)}
+selected=${selected:?no target}
 
-selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
+name=$(basename "$selected" | tr . _)
 
-if [[ -z $TMUX && -z $tmux_running ]]; then
-	tmux new-session -s $selected_name -c $selected
-	exit 0
-fi
-
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-	tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t=$name 2> /dev/null; then
+	tmux new-session -d -s $name -c $selected
 fi
 
 if [[ -z $TMUX ]]; then
-    tmux attach-session -t $selected_name
+	tmux attach-session -t $name
 else
-    tmux switch-client -t $selected_name
+	tmux switch-client -t $name
 fi
