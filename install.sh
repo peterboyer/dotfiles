4b61c4d355abf2ecc98fb63dfb2d601d1f6c92b5
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-03-13 22:50:19 +1100

    fix: flake reference only to dir

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # - TARGET: "home" or "work"
 
 if [ "$1" == "home" ]; then
-	sudo nixos-rebuild switch --flake ./nix/flake.nix#home --impure ${@:2}
+	sudo nixos-rebuild switch --flake ./nix#home --impure ${@:2}
 elif [ "$1" == "work" ]; then
 	darwin-rebuild switch --flake ./nix/flake.nix#work ${@:2}
 else
