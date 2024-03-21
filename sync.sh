9c7cb1ca853132bc3f7c32274e043c915414648f
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-03-21 19:57:53 +1100

    feat: rename rebuild.sh to sync.sh, always run dotbot, cond. os setup

--- sync.sh
+++ sync.sh
@@ -3,16 +3,13 @@
 # usage: $0 <TARGET> [...]
 # - TARGET: "home" or "work"
 
-set -euo pipefail
+set -eo pipefail
 
-TARGET="${1:?Missing target argument.}"
-
+TARGET="$1"
 if [ "$TARGET" == "home" ]; then
 	sudo nixos-rebuild switch --flake ./os#home --impure
-	dotbot -c ./dotbot.yaml
 elif [ "$TARGET" == "work" ]; then
 	brew bundle install --file=./os/user.packages.brewfile
-	dotbot -c ./dotbot.yaml
-else
-	echo "Error: Invalid target."
 fi
+
+dotbot -c ./dotbot.yaml
