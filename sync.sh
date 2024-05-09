c0ec6f5bbd6eb659dc724bf8fb4c6b3a0c4fa8b5
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-05-09 18:12:22 +1000

    feat: dotbot, home/work; sync, bootstrap brew/omz

--- sync.sh
+++ sync.sh
@@ -8,9 +8,21 @@ set -eo pipefail
 TARGET="$1"
 if [ "$TARGET" == "home" ]; then
 	sudo nixos-rebuild switch --flake ./os#home --impure
+	dotbot -c ./dotbot.yaml
+	dotbot -c ./dotbot.home.yaml
 elif [ "$TARGET" == "work" ]; then
+	# autoinstall homebrew
+	set +e; $(brew --version &>/dev/null); has_brew=$?; set -e;
+	if [[ $has_brew != 0 ]]; then
+		echo installing homebrew ...
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+	# autoinstall oh-my-zsh
+	if [[ ! -d ~/.oh-my-zsh ]]; then
+		echo installing oh-my-zsh ...
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	fi
 	brew bundle install --file=./os/user.packages.brewfile
+	dotbot -c ./dotbot.yaml
+	dotbot -c ./dotbot.work.yaml
 fi
-
-export DOTBOT_ENV="$TARGET"
-dotbot -c ./dotbot.yaml
