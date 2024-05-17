d7fb8b2e8bda3e18f7ec3148b7b1ae978650fef2
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-05-17 19:53:14 +1000

    feat(os): sync, add --no-install/link flags

--- sync.sh
+++ sync.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/env bash
 
 # usage: $0 <TARGET> [...]
-# - TARGET: "home" or "work"
+#
+# - <TARGET>: "home" or "work"
+# - --no-install, skips installing packages
+# - --no-link, skips linking dotfiles
 
 set -eo pipefail
 
-TARGET="$1"
-if [ "$TARGET" == "home" ]; then
-	sudo nixos-rebuild switch --flake ./os#home --impure
-	dotbot -c ./dotbot.yaml
-	dotbot -c ./dotbot.home.yaml
-elif [ "$TARGET" == "work" ]; then
-	# autoinstall homebrew
-	set +e; $(brew --version &>/dev/null); has_brew=$?; set -e;
-	if [[ $has_brew != 0 ]]; then
-		echo installing homebrew ...
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+TARGET="${1:?fatal: no TARGET}"
+if [[ "$TARGET" == "home" ]]; then
+	if [[ ! "${@}" =~ "--no-install" ]]; then
+		sudo nixos-rebuild switch --flake ./os#home --impure
+	else
+		echo skip: install ...
 	fi
-	# autoinstall oh-my-zsh
-	if [[ ! -d ~/.oh-my-zsh ]]; then
-		echo installing oh-my-zsh ...
-		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	if [[ ! "${@}" =~ "--no-link" ]]; then
+		dotbot -c ./dotbot.yaml
+		dotbot -c ./dotbot.home.yaml
+	else
+		echo skip: link ...
 	fi
-	brew bundle install --file=./os/user.packages.brewfile
-	dotbot -c ./dotbot.yaml
-	dotbot -c ./dotbot.work.yaml
+elif [[ "$TARGET" == "work" ]]; then
+	if [[ ! "${@}" =~ "--no-install" ]]; then
+		# autoinstall homebrew
+		set +e; $(brew --version &>/dev/null); has_brew=$?; set -e;
+		if [[ $has_brew != 0 ]]; then
+			echo installing homebrew ...
+			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		fi
+		# autoinstall oh-my-zsh
+		if [[ ! -d ~/.oh-my-zsh ]]; then
+			echo installing oh-my-zsh ...
+			sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+		fi
+		brew bundle install --file=./os/user.packages.brewfile
+	else
+		echo skip: install ...
+	fi
+	if [[ ! "${@}" =~ "--no-link" ]]; then
+		dotbot -c ./dotbot.yaml
+		dotbot -c ./dotbot.work.yaml
+	else
+		echo skip: link ...
+	fi
+else
+	echo fatal: invalid TARGET: $TARGET
+	exit 1
 fi
