ddae90d9684b4bd6327d8596a7a74b2168bdab13
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-06-14 18:13:51 +1000

    fix(nvim): ts compiler use npx

--- nvim/lua/config/filetypes.lua
+++ nvim/lua/config/filetypes.lua
@@ -2,7 +2,7 @@
 -- https://strdr4605.com/typescript-errors-into-vim-quickfix
 vim.cmd([[
   augroup strdr4605
-    autocmd FileType typescript,typescriptreact compiler tsc | setlocal makeprg=yarn\ tsc
+    autocmd FileType typescript,typescriptreact compiler tsc | setlocal makeprg=npx\ tsc
   augroup END
 ]])
 
