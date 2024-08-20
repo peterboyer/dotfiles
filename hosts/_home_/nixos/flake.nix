cae051007a6f73d248aa3119f3f0f9b9f46595ed
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-08-20 18:41:16 +1000

    fix(hosts.home): nixpkgs/pinned/unstable

--- hosts/_home_/nixos/flake.nix
+++ hosts/_home_/nixos/flake.nix
@@ -6,22 +6,25 @@
 		nixpkgs-unstable = {
 			url = "github:nixos/nixpkgs/nixos-unstable";
 		};
+		nixpkgs-pinned = {
+			url = "github:nixos/nixpkgs/12bf09802d77264e441f48e25459c10c93eada2e";
+		};
 	};
 
-	outputs = { nixpkgs, nixpkgs-unstable, ... }:
+	outputs = { nixpkgs, nixpkgs-unstable, nixpkgs-pinned, ... }:
 	let
 		system = "x86_64-linux";
 		options = { inherit system; config.allowUnfree = true; };
 		pkgs = import nixpkgs options;
 		pkgs-unstable = import nixpkgs-unstable options;
-		pkgs-custom = import ./pkgs-custom pkgs;
+		pkgs-pinned = import nixpkgs-pinned options;
 	in
 	{
 		nixosConfigurations.default = nixpkgs.lib.nixosSystem {
 			inherit system;
 			specialArgs = {
-				inherit pkgs-unstable;
-				inherit pkgs-custom;
+				pkgs-unstable = pkgs-unstable;
+				pkgs-custom = import ./pkgs-custom pkgs-pinned;
 			};
 			modules = [
 				./hardware-configuration.nix
