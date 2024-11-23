caae39ce629485c9ae382dd29f9183ec2e91a389
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-11-23 13:52:56 +1100

    feat(hosts.laptop): add host-* bin links

--- hosts/laptop/links.sh
+++ hosts/laptop/links.sh
@@ -11,23 +11,26 @@ function main() {
 
 	link ~/.tmux.conf           ~/_/dotfiles/tmux/tmux.conf
 	link ~/.config/nvim         ~/_/dotfiles/nvim
-	link ~/.local/share/nvim    ~/_/local/share/nvim
 	link ~/.config/lazygit      ~/_/dotfiles/lazygit
 	link ~/.config/kitty        ~/_/dotfiles/kitty
+
+	link ~/.local/bin/host-update    ~/_/dotfiles/hosts/laptop/update.sh
+	link ~/.local/bin/host-links     ~/_/dotfiles/hosts/laptop/links.sh
+	link ~/.local/bin/host-packages  ~/_/dotfiles/hosts/laptop/packages.sh
 
 	link /usr/local/share/fonts/berkeley-mono ~/_/fonts/berkeley-mono --sudo
 }
 
 function link() {
 	if [[ ${FORCE:-} != 'yes' && -e $1 ]]; then
-		echo "[skip] $1 => $2"
+		echo -e "[\033[37mskip\033[0m] $1 => $2"
 		return
 	fi
 	sudoexec=$(if [[ ${@} =~ "--sudo" ]]; then echo "sudo"; fi)
 	$sudoexec rm -rf $1 || true
 	$sudoexec mkdir -p $(dirname $1)
 	(cd $(dirname $1) && $sudoexec ln -sf $2 $(basename $1))
-	echo "[link] $1 => $2"
+	echo -e "[\033[32mlink\033[0m] $1 => $2"
 }
 
 main
