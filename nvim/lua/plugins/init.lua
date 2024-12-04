f21e3014c8333c70e02da46fd3ede8a760d421c9
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-12-04 22:49:15 +1100

    feat(nvim): remove lexima.vim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,8 +7,6 @@ return {
 	},
 	-- toggle quickfix/locationlist
 	{ "milkypostman/vim-togglelist" },
-	-- auto closing bracket/quotes
-	{ "cohama/lexima.vim" },
 	-- add/change/delete surrounding brackets/quotes/tags
 	{ "tpope/vim-surround" },
 	-- git tooling (blame)
