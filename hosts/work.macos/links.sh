fb75c7bf7e46e518eabe1a56d8137460cc61d3b2
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-12-21 19:17:41 +1100

    fix(hosts.work): link ordering, add host-links alias

--- hosts/work.macos/links.sh
+++ hosts/work.macos/links.sh
@@ -2,10 +2,13 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+. ~/_/dotfiles/links.sh
+
+dotlink ~/.local/bin/host-links ~/_/dotfiles/hosts/work.macos/links.sh
+
 dotlink ~/.zshrc      ~/_/dotfiles/zsh/zshrc.macos
 dotlink ~/.gitconfig  ~/_/dotfiles.untracked/git/gitconfig
 dotlink ~/.npmrc      ~/_/dotfiles.untracked/npm/npmrc
 dotlink ~/.yarnrc     ~/_/dotfiles.untracked/yarn/yarnrc
 
-. ~/_/dotfiles/links.sh
 dotlink ~/.config/ngrok/ngrok.yml ~/_/dotfiles.untracked/ngrok/ngrok.yml
