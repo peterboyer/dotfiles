424a26fe1bd93d804849190901dfff4c8888760c
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2025-02-18 20:19:06 +1100

    fix(hosts.home): signal symlink

--- hosts/home.fedora/links.sh
+++ hosts/home.fedora/links.sh
@@ -9,7 +9,7 @@ dotlink ~/.local/bin/host-packages  ~/_/dotfiles/hosts/home.fedora/packages.sh
 dotlink ~/.local/share/fonts/berkeley-mono ~/_/fonts/berkeley-mono
 dotlink ~/.local/share/fonts/apple-fonts ~/_/fonts/apple-fonts
 
-dotlink ~/.config/Signal ~/_/dotfiles.untracked/signal
+dotlink ~/.var/app/org.signal.Signal/config/Signal ~/_/dotfiles.untracked/signal
 
 dotlink /etc/samba/smb.conf ~/_/dotfiles/samba/smb.conf --sudo --copy
 # sudo systemctl restart smb.service
