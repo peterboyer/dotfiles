c716afc97ea797362fffdb8f78731afe85833928
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2025-04-03 15:38:35 +1100

    fix(nvim): nerdcommenter keys

--- nvim/lua/plugins/nerdcommenter.lua
+++ nvim/lua/plugins/nerdcommenter.lua
@@ -1,9 +1,4 @@
-local keys = {
-	{ "<leader>c<leader>", ":<Plug>NERDCommenterToggle", silent = true, desc = "NerdCommenter Toggle" },
-}
-
 --
-vim.g.NERDCreateDefaultMappings = 0
 vim.g.NERDSpaceDelims = 1
 vim.g.NERDDefaultAlign = "left"
 --
@@ -11,6 +6,5 @@ vim.g.NERDDefaultAlign = "left"
 return {
 	{
 		"preservim/nerdcommenter",
-		keys = keys,
 	},
 }
